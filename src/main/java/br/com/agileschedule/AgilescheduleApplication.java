c820c650e4a33d518e4a63d822ada0e3870868fd
Andrey <83211338+nizzorto@users.noreply.github.com>
2021-06-14 12:57:22 -0300

    Add files via upload

---
+++ src/main/java/br/com/agileschedule/AgilescheduleApplication.java
@@ -0,0 +1,13 @@
+package br.com.agileschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgilescheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgilescheduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/agileschedule/AgilescheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.agileschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgilescheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
